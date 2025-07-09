5b5f5f05ddbe183e1df833d3755f664f2c4b6ab6
Nikushaana <nikanashvili@gmail.com>
2025-07-09 14:11:44 +0400

    change file size

--- back-end/src/middleware/upload.js
+++ back-end/src/middleware/upload.js
@@ -8,8 +8,8 @@ const VideoDir = path.join(__dirname, "..", "..", "uploads", "videos");
 fs.mkdirSync(ImageDir, { recursive: true });
 fs.mkdirSync(VideoDir, { recursive: true });
 
-// const MAX_FILE_SIZE = 3 * 1024 * 1024;
-const MAX_FILE_SIZE = 10 * 1024;
+const MAX_IMAGE_SIZE = 1 * 1024 * 1024;
+const MAX_VIDEO_SIZE = 10 * 1024 * 1024;
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -24,9 +24,14 @@ const storage = multer.diskStorage({
 });
 
 const fileFilter = (req, file, cb) => {
-  if (file.size > MAX_FILE_SIZE) {
-    return cb(new Error("File size should not exceed 3 MB"), false);
+  if (file.mimetype.startsWith("image/") && file.size > MAX_IMAGE_SIZE) {
+    return cb(new Error("Image size should not exceed 100 KB"), false);
   }
+
+  if (file.mimetype.startsWith("video/") && file.size > MAX_VIDEO_SIZE) {
+    return cb(new Error("Video size should not exceed 3 MB"), false);
+  }
+
   if (
     file.mimetype.startsWith("image/") ||
     file.mimetype.startsWith("video/")
@@ -38,7 +43,7 @@ const fileFilter = (req, file, cb) => {
 };
 
 const limits = {
-  fileSize: MAX_FILE_SIZE,
+  fileSize: MAX_VIDEO_SIZE,
 };
 
 module.exports = {
